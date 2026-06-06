fe59004715551a29a4d022093ba603b22b077723
Razor-Z-Pi <popovp.popoff2001@gmail.com>
2026-06-06 13:30:31 +0700

    fix and update Prct№4

--- Neural networks and deep learning/Practik№4/index.py
+++ Neural networks and deep learning/Practik№4/index.py
@@ -1,125 +1,41 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def sigmoid(x):
-    # Сигмоидальная функция активации: f(x) = 1 / (1 + e^(-x))
-    return 1 / (1 + np.exp(-x))
+# Создание обучающего набора данных с символами
+symbols = {
+    'L': np.array([[1, 0, 0],  # Определяем символ 'L' как 3x3 массив
+                   [1, 0, 0],  # Вторая строка
+                   [1, 1, 1]]),  # Третья строка
+    'T': np.array([[1, 1, 1],  # Определяем символ 'T'
+                   [0, 1, 0],  # Вторая строка
+                   [0, 1, 0]]),  # Третья строка
+    'X': np.array([[1, 0, 1],  # Определяем символ 'X'
+                   [0, 1, 0],  # Вторая строка
+                   [1, 0, 1]])  # Третья строка
+}
 
-def deriv_sigmoid(x):
-    # Производная сигмоиды: f'(x) = f(x) * (1 - f(x))
-    fx = sigmoid(x)
-    return fx * (1 - fx)
+# Функция для вычисления Хэммингова расстояния
+def hamming_distance(a, b):
+    return np.sum(a != b)
 
-def mse_loss(y_true, y_pred):
-    # y_true и y_pred - numpy массивы одинаковой длины.
-    return ((y_true - y_pred) ** 2).mean()
+# Функция для распознавания символа
+def recognize_symbol(input_symbol):
+    distances = {}
+    for symbol, template in symbols.items():
+        distances[symbol] = hamming_distance(input_symbol, template)
 
-class OurNeuralNetwork:
-    '''
-    Нейронная сеть с:
-      - 2 входными
-      - скрытым слоем с 2 нейронами (h1, h2)
-      - выходным слоем с 1 нейроном (o1)
-    '''
-    def __init__(self):
-        # Веса
-        self.w1 = np.random.normal()
-        self.w2 = np.random.normal()
-        self.w3 = np.random.normal()
-        self.w4 = np.random.normal()
-        self.w5 = np.random.normal()
-        self.w6 = np.random.normal()
+    # Находим символ с минимальным расстоянием
+    recognized_symbol = min(distances, key = distances.get)
+    return recognized_symbol
 
-        # Смещения (Biases)
-        self.b1 = np.random.normal()
-        self.b2 = np.random.normal()
-        self.b3 = np.random.normal()
+input_symbol = np.array([[0, 0, 0],
+                         [1, 0, 0],
+                         [1, 1, 0]])
+recognized = recognize_symbol(input_symbol)
+print(f'Распознанный символ: {recognized}')
 
-    def feedforward(self, x):
-        # x is a numpy array with 2 elements.
-        h1 = sigmoid(self.w1 * x[0] + self.w2 * x[1] + self.b1)
-        h2 = sigmoid(self.w3 * x[0] + self.w4 * x[1] + self.b2)
-        o1 = sigmoid(self.w5 * h1 + self.w6 * h2 + self.b3)
-        return o1
-
-    def train(self, data, all_y_trues):
-        '''
-        data - массив numpy (n x 2), n = x-координаты выборки.
-        all_y_trues - массив numpy (n x 1).
-        '''
-        learn_rate = 0.1
-        epochs = 1000 # Сколько раз пройти по всему набору данных
-
-        for epoch in range(epochs):
-            for x, y_true in zip(data, all_y_trues):
-                # Прямой проход (эти значения нам понадобятся позже)
-                sum_h1 = self.w1 * x[0] + self.w2 * x[1] + self.b1
-                h1 = sigmoid(sum_h1)
-
-                sum_h2 = self.w3 * x[0] + self.w4 * x[1] + self.b2
-                h2 = sigmoid(sum_h2)
-
-                sum_o1 = self.w5 * h1 + self.w6 * h2 + self.b3
-                o1 = sigmoid(sum_o1)
-                y_pred = o1
-
-                # Частные производные
-                # d_L_d_ypred = -2 * (y_true - y_pred)
-                d_L_d_ypred = -2 * (y_true - y_pred)
-
-                # Нейрон o1
-                d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
-                d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
-                d_ypred_d_b3 = deriv_sigmoid(sum_o1)
-
-                d_ypred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
-                d_ypred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
-
-                # Нейрон h1
-                d_h1_d_w1 = x[0] * deriv_sigmoid(sum_h1)
-                d_h1_d_w2 = x[1] * deriv_sigmoid(sum_h1)
-                d_h1_d_b1 = deriv_sigmoid(sum_h1)
-
-                # Нейрон h2
-                d_h2_d_w3 = x[0] * deriv_sigmoid(sum_h2)
-                d_h2_d_w4 = x[1] * deriv_sigmoid(sum_h2)
-                d_h2_d_b2 = deriv_sigmoid(sum_h2)
-
-                # Обновляем веса и пороги
-                # Нейрон h1
-                self.w1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
-                self.w2 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
-                self.b1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_b1
-
-                # Нейрон h2
-                self.w3 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w3
-                self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
-                self.b2 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2
-
-                # Нейрон o1
-                self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
-                self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
-                self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
-
-            # Считаем полные потери в конце каждой эпохи
-            if epoch % 10 == 0:
-                y_preds = np.apply_along_axis(self.feedforward, 1, data)
-                loss = mse_loss(all_y_trues, y_preds)
-                print("Эпоха %d Потерь: %.3f" % (epoch, loss))
-
-# Определим набор данных
-data = np.array([
-    [-2, -1], # Алиса
-    [25, 6],  # Боб
-    [17, 4],  # Чарли
-    [-15, -6], # Диана
-])
-all_y_trues = np.array([
-    1, # Алиса
-    0, # Боб
-    0, # Чарли
-    1, # Диана
-])
-
-# Обучаем нашу нейронную сеть!!!
-network = OurNeuralNetwork()
-network.train(data, all_y_trues)
+# Визуализация
+fig, ax = plt.subplots()  # Создаем фигуру и оси для графика
+ax.imshow(input_symbol, cmap = 'gray', vmin = 0, vmax = 1)  # Отображаем входной символ с цветовой картой
+ax.set_title('Входной символ')
+plt.show()
